29273ccfcb4bce7d2aa87cb08f3d2b9a4ae26ac8
ZWMDR <953799126lxc@gmail.com>
2019-05-15 15:52:58 +0800

    增加文件收发功能

--- chat_window.cpp
+++ chat_window.cpp
@@ -101,6 +101,38 @@ void Chat_Window::socket_recv()
         this->IP->socket->readAll();
         QObject::connect(IP->socket,&QTcpSocket::readyRead,this,&Chat_Window::socket_recv);
     }
+    else if(strncmp(buffer,"SYS_SIGNAL_FILE:",15)==0)
+    {
+        QString cmd=buffer;
+        QStringList cmd_split=cmd.split(":");
+        qint64 counts=cmd_split[2].toLong();
+        std::cout<<"counts="<<counts<<std::endl;
+        QString file_name=cmd_split[1];
+        QFile file(file_name);
+        file.open(QIODevice::WriteOnly);
+        std::cout<<"file opened"<<std::endl;
+        QObject::disconnect(IP->socket,&QTcpSocket::readyRead,this,&Chat_Window::socket_recv);
+        while(counts>0)
+        {
+            clear_buf(buffer);
+            IP->recved=IP->socket->waitForReadyRead(1);
+            qint64 recv_len=this->IP->socket->read(buffer,SIZE);
+            file.write(buffer,recv_len);
+            counts-=recv_len;
+        }
+        file.close();
+        QString msg;
+        for(int i=3;i<cmd_split.length();i++)
+        {
+            msg+=cmd_split[i];
+            if(i<cmd_split.length())
+                msg+=":";
+        }
+        this->ui->output->append(msg+"\n");
+
+        this->IP->socket->readAll();
+        QObject::connect(IP->socket,&QTcpSocket::readyRead,this,&Chat_Window::socket_recv);
+    }
     else
     {
         strcat(buffer,"\n");
@@ -162,6 +194,65 @@ void Chat_Window::on_file_btn_clicked()//文件
     for(auto tmp:fileNames)//获取文件完整路径
     {
         qDebug()<<tmp<<endl;
+        //获取文件信息
+        QFileInfo info(tmp);
+        qint64 file_size=info.size();//字节
+        std::cout<<"img_size="<<(QString::number((double)file_size/1024,10,2)+"KB,").toStdString()<<std::endl;
+        if(file_size/1024/1024>20)//最大不超过20MB
+        {
+            QMessageBox msg;
+            msg.setWindowTitle("提示");
+            QString mesg="所选文件大小为:"+QString::number(file_size/1024.0/1024.0,10,2)+"KB,"+"文件太大无法发送!";
+            msg.setText(mesg);
+            msg.setStyleSheet("font: 8pt;");
+            msg.setIcon((QMessageBox::Information));
+            msg.addButton(tr("确定"),QMessageBox::ActionRole);
+            msg.exec();
+            continue;
+        }
+        qint64 counts=file_size/1024+1;
+        //打开文件
+        QFile file(tmp);
+        if(!file.open(QIODevice::ReadOnly))
+        {
+            QMessageBox msg;
+            msg.setWindowTitle("提示");
+            msg.setText("文件打开错误，请稍后重试！");
+            msg.setStyleSheet("font: 8pt;");
+            msg.setIcon((QMessageBox::Information));
+            msg.addButton(tr("确定"),QMessageBox::ActionRole);
+            msg.exec();
+            continue;
+        }
+        QDataStream read_in(&file);
+        //提取文件名
+        QDateTime Time = QDateTime::currentDateTime();//获取系统现在的时间
+        QString time = Time.toString("yyyy_MM_dd_hh_mm_ss_ddd");
+        QStringList img_name = tmp.split("/");
+        QString fl_name=img_name.last();
+        //QString fl_dump=send_imgs+time+fl_name;
+        //QFile file_dump(fl_dump);
+        //file_dump.open(QIODevice::WriteOnly);
+        std::cout<<fl_name.toStdString()<<std::endl;
+        //发送图片
+        clear_buf(buffer);
+        strcpy(buffer,"SYS_SIGNAL_FILE:");//系统文件发送信号
+        strcat(buffer,(fl_name+":").toStdString().c_str());//图片名
+        strcat(buffer,QString::number(file_size,10).toStdString().c_str());//发送字节数
+        counts=file_size;
+        IP->socket->write(buffer,SIZE);
+        IP->socket->waitForBytesWritten(2000);
+        while(counts>0)
+        {
+            clear_buf(buffer);
+            file.read(buffer,SIZE);
+            qint64 send_len=this->IP->socket->write(buffer,SIZE);
+            this->IP->sended=this->IP->socket->waitForBytesWritten(1000);
+            //file_dump.write(buffer,SIZE);
+            counts-=send_len;
+        }
+        file.close();
+        //file_dump.close();
     }
 }
 
@@ -217,9 +308,9 @@ void Chat_Window::on_img_btn_clicked()
         QString time = Time.toString("yyyy_MM_dd_hh_mm_ss_ddd");
         QStringList img_name = tmp.split("/");
         QString im_name=img_name.last();
-        QString im_dump=send_imgs+time+im_name;
-        QFile img_dump(im_dump);
-        img_dump.open(QIODevice::WriteOnly);
+        //QString im_dump=send_imgs+time+im_name;
+        //QFile img_dump(im_dump);
+        //img_dump.open(QIODevice::WriteOnly);
         std::cout<<im_name.toStdString()<<std::endl;
         //发送图片
         clear_buf(buffer);
@@ -229,19 +320,16 @@ void Chat_Window::on_img_btn_clicked()
         counts=img_size;
         IP->socket->write(buffer,SIZE);
         IP->socket->waitForBytesWritten(2000);
-        //std::cout<<"send buffer:"<<buffer<<std::endl;
         while(counts>0)
         {
             clear_buf(buffer);
             file.read(buffer,SIZE);
             qint64 send_len=this->IP->socket->write(buffer,SIZE);
             this->IP->sended=this->IP->socket->waitForBytesWritten(1000);
-            //this->IP->recved=this->IP->socket->waitForReadyRead(1000);
-            //this->IP->socket->read(buff,SIZE);
-            img_dump.write(buffer,SIZE);
+            //img_dump.write(buffer,SIZE);
             counts-=send_len;
         }
         file.close();
-        img_dump.close();
+        //img_dump.close();
     }
 }
